ca05963c9e4c5b81e18cb3f1c02712bc57af2363
Dzmitry Sudalenka <sudalenko@gmail.com>
2023-11-17 23:10:09 +0300

    feat: add removeFirstOccurrences task solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,12 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const valueIndex = str.indexOf(value);
+  if (valueIndex !== -1) {
+    return str.slice(0, valueIndex) + str.slice(valueIndex + value.length);
+  }
+  return str;
 }
 
 /**
